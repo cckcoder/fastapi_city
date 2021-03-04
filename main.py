cb3d42e4b3142dee07bee4273dab3cf59dbc97ab
codewiz <c.chaiwat.ch@gmail.com>
2021-03-04 07:34:43 +0700

    add: tortoise-orm

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
-
+from tortoise import fields
+from tortoise.models import Model
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 db = []
 app = FastAPI()
 
-class City(BaseModel):
-    name: str
-    timezone: str
+
+class City(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(50, unique=True)
+    timezone = fields.CharField(50)
+
+
+city_pydantic = pydantic_model_creator(City, name='City')
+cityin_pydantic = pydantic_model_creator(City, name="CityIn", exclude_readonly=True)
 
 
 @app.get('/')
 def index():
     return {'key': 'value'}
+
 
 @app.get('/cities')
 def get_cities():
@@ -30,22 +40,32 @@ def get_cities():
 
 @app.get('/cities/{city_id}')
 def get_city(city_id: int):
-    city = db[city_id - 1] 
+    city = db[city_id - 1]
     r = requests.get(f'http://worldtimeapi.org/api/timezone/{city.timezone}')
     current_time = r.json()['datetime']
     city_data = {
         'name': city.name,
         'time zone': city.timezone,
         'current_time': current_time
-    } 
+    }
     return city_data
 
 @app.post('/cities')
-def create_city(city: City):
-    db.append(city)
-    return db[-1]
+async def create_city(city: cityin_pydantic):
+    city_obj = await City.create(**city.dict(exclude_unset=True))
+    return await city_pydantic.from_tortoise_orm(city_obj)
+
 
 @app.delete('/cities/{city_id}')
 def delete_city(city_id: int):
     db.pop(city_id - 1)
-    return {}   
+    return {}
+
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
